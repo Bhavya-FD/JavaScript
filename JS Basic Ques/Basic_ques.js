02d3b9d4d6092886ea9dbe8e71cb9547d0c39b5f
Bhavya-FD <kotharibhavya30@gmail.com>
2024-02-15 12:08:24 +0530

    JavaScript

--- JS Basic Ques/Basic_ques.js
+++ JS Basic Ques/Basic_ques.js
@@ -172,4 +172,34 @@ function printStatement() {
     console.log('Largest no. is:' , Math.max(20,30,10));
   }
 
+  // Q.28(i) Week Days
+  function weekDay() {
+    let Day = Number(document.getElementById('week').value);
+
+    switch (Day) {
+      case 1:
+        alert("It's Monday!")
+        break;
+      case 2:
+        alert("It's Tuesday!")
+        break;
+      case 3:
+        alert("It's Wednesday!")
+        break;  
+      case 4:
+        alert("It's Thursday!")
+        break;  
+      case 5:
+        alert("It's Friday!")
+        break;
+      case 6:
+        alert("It's Saturday!")
+        break;  
+      case 7:
+        alert("It's Sunday!")
+        break;  
+      default:
+        break;
+    }
+  }
 
